1eb75e10f224893cb200b38f617f45620b44c04f
gongshunkai <49078111@qq.com>
2017-05-03 13:56:41 +0800

    scrollpagination

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
-# demo-jquery.scrollpagination
-jquery插件-无限滚动翻页
+﻿<h2># demo-jquery.scrollpagination v1.0</h2>
+<p>jquery插件 ( 无限滚动翻页  ) - jquery.scrollpagination v1.0</p>
+<p>支持横向滚动与垂直滚动</p>
+<p><b>演示地址：</b><a href="http://gongshunkai.github.io/demo/%E6%97%A0%E9%99%90%E6%BB%9A%E5%8A%A8%E7%BF%BB%E9%A1%B5/demo.html">http://gongshunkai.github.io/demo/%E6%97%A0%E9%99%90%E6%BB%9A%E5%8A%A8%E7%BF%BB%E9%A1%B5/demo.html</a></p>
+<p><b>使用方法：</b></p>
+<p>$('.product-list').scrollPagination({<br>&nbsp;&nbsp;
+url:function(page){<br>&nbsp;&nbsp;
+&nbsp; &nbsp; return 'data/demo.json?page=' + page;<br>&nbsp;&nbsp;
+},<br>&nbsp;&nbsp;
+beforeLoad: function(){<br>&nbsp;&nbsp;
+&nbsp; &nbsp; $('#loading').fadeIn();<br>&nbsp;&nbsp;
+},<br>&nbsp;&nbsp;
+afterLoad:function(data){<br>&nbsp;&nbsp;
+&nbsp; &nbsp; $('#loading').fadeOut();<br>&nbsp;&nbsp;
+&nbsp; &nbsp; $.each(data,function(i,o){<br>&nbsp;&nbsp;
+&nbsp; &nbsp; &nbsp; &nbsp; $('.product-list').append($('&lt;div class=&quot;thumbnail&quot;&gt;&lt;img src=&quot;' + o.thumb + '&quot; alt=&quot;' + o.name + '&quot;&gt;&lt;/div&gt;'));<br>&nbsp;&nbsp;
+&nbsp; &nbsp; });<br>&nbsp;&nbsp;
+},<br>&nbsp;&nbsp;
+pageIndex:5,<br>
+&nbsp;&nbsp;
+offset:10<br>
+});</p>
+<p>初始化提供1个配置项参数</p>
+<p>{<br>
+  &nbsp;&nbsp;&nbsp;&nbsp;
+  url:null, //数据<br>&nbsp;&nbsp;&nbsp;&nbsp;
+  postData:null, //发送数据<br>&nbsp;&nbsp;&nbsp;&nbsp;
+  beforeLoad:null, //请求前的回调<br>&nbsp;&nbsp;&nbsp;&nbsp;
+  afterLoad:null, //请求后的回调<br>&nbsp;&nbsp;&nbsp;&nbsp;
+  onError:null, //错误的回调<br>&nbsp;&nbsp;&nbsp;&nbsp;
+  finished:null, //完成的回调<br>&nbsp;&nbsp;&nbsp;&nbsp;
+  scrollTarget:null, //滚动目标<br>&nbsp;&nbsp;&nbsp;&nbsp;
+  scrollPage:null, //滚动翻页<br>&nbsp;&nbsp;&nbsp;&nbsp;
+  pageIndex:null, //当前页码<br>&nbsp;&nbsp;&nbsp;&nbsp;
+  pageCount:null, //总页码<br>&nbsp;&nbsp;&nbsp;&nbsp;
+  offset:null, //偏移<br>&nbsp;&nbsp;&nbsp;&nbsp;
+  vertical:null, //是否垂直<br>
+  };</p>
---
+++ jquery.scrollpagination.js
@@ -0,0 +1,94 @@
+﻿(function( $ ){
+
+	$.fn.scrollPagination = function(options) {
+
+		var opts = $.extend({},$.fn.scrollPagination.defaults, options);  
+		var target = opts.scrollTarget;
+		if (target == null) target = obj;
+		opts.scrollTarget = target;
+
+		return this.each(function() {
+			$.fn.scrollPagination.init($(this), opts);
+		});
+
+	};
+
+	$.fn.stopScrollPagination = function(){
+		return this.each(function() {
+			$(this).attr('scrollPagination', 'disabled');
+		});
+	};
+
+	$.fn.setScrollPagination = function(pageIndex,pageCount){
+		if(typeof pageIndex === 'object'){
+			var obj = pageIndex || {};
+			pageIndex = obj.pageIndex;
+			pageCount = obj.pageCount;
+		}
+		return this.each(function() {
+			pageIndex && $(this).attr('pageIndex',pageIndex);
+			pageCount && $(this).attr('pageCount',pageCount);
+		});
+	};
+
+	$.fn.scrollPagination.loadContent = function(obj, opts){
+		var target = opts.scrollTarget,
+			page = opts.scrollPage,
+			vertical = opts.vertical;
+
+		var mayLoadContent = $(target)[vertical ? 'scrollTop' : 'scrollLeft']() + opts.offset >= $(page)[vertical ? 'outerHeight' : 'outerWidth'](true) - $(target)[vertical ? 'outerHeight' : 'outerWidth'](true);
+		if (mayLoadContent){
+
+			var pageIndex = parseInt($(obj).attr('pageIndex')),
+				pageCount = parseInt($(obj).attr('pageCount')),
+				url = typeof opts.url === 'function' ? opts.url(pageIndex) : opts.url;
+
+			var ajaxCallback = function(data){
+				opts.afterLoad && opts.afterLoad(data);
+				if(pageCount && pageIndex >= pageCount)
+					opts.finished && opts.finished(pageIndex);
+				else
+					$(obj).attr({'pageIndex':++pageIndex,'scrollPagination':'enabled'});
+			};
+
+			if(opts.beforeLoad && opts.beforeLoad() === false) return;
+
+			$(obj).attr('scrollPagination', 'disabled');
+
+			if(opts.postData)
+				$.post(url, opts.postData, ajaxCallback, "json").error(opts.onError);
+			else
+				$.getJSON(url, ajaxCallback).error(opts.onError);
+		}
+
+	};
+
+	$.fn.scrollPagination.init = function(obj, opts){
+		var target = opts.scrollTarget;
+		$(obj).attr({'scrollPagination':'enabled','pageIndex':opts.pageIndex,'pageCount':opts.pageCount});
+
+		$(target).scroll(function(event){
+			if ($(obj).attr('scrollPagination') == 'enabled')
+				$.fn.scrollPagination.loadContent(obj, opts);
+			else 
+				event.stopPropagation();
+		});
+
+		$.fn.scrollPagination.loadContent(obj, opts);
+	};
+
+	$.fn.scrollPagination.defaults = {
+		'url' : null,
+		'postData' : null,
+		'onError' : null,
+		'beforeLoad' : null,
+		'afterLoad' : null,
+		'finished' : null,
+		'scrollTarget': window,
+		'scrollPage': document,
+		'pageIndex' : 1,
+		'pageCount' : null,
+		'offset' : 0,
+		'vertical' : true	  
+	};	
+})( jQuery );
